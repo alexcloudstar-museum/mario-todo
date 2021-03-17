21fa58b5d9f3b293a080adf72bd868576b48bb83
alexcloudstar <alexcloudstar@gmail.com>
2021-03-17 09:01:17 +0200

    Add .env.template file && set headers for app

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,11 +6,25 @@ import express, {
   json,
 } from 'express';
 
+import dotenv from 'dotenv';
+
 const app: express.Application = express();
-
 app.use(urlencoded({ extended: true }));
-
 app.use(json());
+dotenv.config();
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
+app.use('/api');
 
 app.get('/', (req: Request, res: Response, next: NextFunction) =>
   res.send('Express + TypeScript Server')
